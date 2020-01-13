d9b925efa7ccec0fa8675d2cacb928f40b79ad0a
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2020-01-13 09:55:00 -0600

    Ejercicio 1: Sensor de proximidad de carro

---
+++ Arduino Intermedio/Ejercicios/Ejercicio_1_-_Sensor_de_proximidad_de_carro/Ejercicio_1_-_Sensor_de_proximidad_de_carro.ino
@@ -0,0 +1,38 @@
+// Se nombran los pines
+int trig = 8;
+int echo = 9;
+
+int buzzer = 7;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  long distancia;
+  long tiempo;
+  
+  // Para activar el sensor
+  digitalWrite(trig,LOW);
+  delayMicroseconds(4);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+  // Operaciones
+  tiempo = pulseIn(echo,HIGH);  // Para recibir la información del sensor
+  tiempo = tiempo/2;
+  distancia = tiempo/29.2;
+  Serial.print(distancia);
+  Serial.println(" cm");
+  
+  if(distancia <= 10){
+    digitalWrite(buzzer,HIGH);
+    delay(500);
+    digitalWrite(buzzer,LOW);
+  }
+  delay(500);
+}
