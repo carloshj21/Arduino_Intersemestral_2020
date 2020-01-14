5a6784c183d6a76326d9a37ced3267fadab96326
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2020-01-14 09:59:21 -0600

    Interrupción con sensor ultrasonico y botón

---
+++ Arduino Intermedio/Ejemplos/2. Interrupciones/Interrupcion_con_sensor_ultrasonico_y_boton/Interrupcion_con_sensor_ultrasonico_y_boton.ino
@@ -0,0 +1,43 @@
+#define trig 8
+#define echo 9
+
+int contador = 0;
+
+long distancia;
+
+void setup() {
+  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt(3),modContador,FALLING);  // Para hacer la interrupción
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
+}
+
+void loop() {
+  
+  long tiempo;
+
+  // Para activar el sensor
+  digitalWrite(trig,LOW);
+  delayMicroseconds(4);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig,LOW);
+
+
+  // Operaciones
+  tiempo = pulseIn(echo,HIGH);  // Para recibir la información del sensor
+  tiempo = tiempo/2;
+  distancia = tiempo/29.2;
+  Serial.print(distancia);
+  Serial.println(" cm");
+  delay(500);
+}
+
+void modContador(){
+  if(distancia <= 30){
+    contador = contador + 1;
+  }else{
+    contador = contador - 1;
+  }
+  Serial.println(contador);
+}
