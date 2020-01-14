59f50af39c41c745df2f057607c6cc93ad8d4633
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2020-01-14 09:23:36 -0600

    Función attachInterrupt()

---
+++ Arduino Intermedio/Ejemplos/2. Interrupciones/attachInterrupt/attachInterrupt.ino
@@ -0,0 +1,25 @@
+#define button 3
+#define led 13
+
+int contador;
+bool led_state = 0;
+bool last_state;
+
+void setup() {
+  attachInterrupt(digitalPinToInterrupt(3),prende,FALLING);  // Para hacer la interrupción
+  pinMode(button,INPUT_PULLUP);
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println(contador);
+  contador++;
+  delay(2000);
+}
+
+void prende(){
+  led_state = !led_state;
+  digitalWrite(led,led_state);
+}
+
