530326951107c38a10b8255effa78338555bc376
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2020-01-17 21:56:19 -0600

    Tarea 4 - Interrupción con sensor ultrasónico y botón

--- Arduino Intermedio/Tareas/Interrupcion_con_sensor_ultrasonico_y_boton/Interrupcion_con_sensor_ultrasonico_y_boton.ino
+++ Arduino Intermedio/Tareas/Interrupcion_con_sensor_ultrasonico_y_boton/Interrupcion_con_sensor_ultrasonico_y_boton.ino
@@ -12,6 +12,9 @@ void setup() {
   pinMode(trig,OUTPUT);
   pinMode(echo,INPUT);
   pinMode(3,INPUT_PULLUP);
+
+  Serial.print("Contador: ");
+  Serial.println(contador);
 }
 
 void loop() {
@@ -28,19 +31,30 @@ void loop() {
   tiempo = pulseIn(echo,HIGH);  // Para recibir la información del sensor
   tiempo = tiempo/2;
   distancia = tiempo/29.2;
-  Serial.print("\nDistancia: ");
-  Serial.print(distancia);
-  Serial.println(" cm");
-  Serial.print("Contador: ");
-  Serial.println(contador);
-
   delay(500);
 }
 
+
+/*  *********************************************************************************************************************************
+ 
+Observación:  Creo que debido al rebote de los botones del que se nos habló en clase, el contador aumenta, aveces, más de una ocasión. 
+
+************************************************************************************************************************************** */
 void modContador(){
   if(distancia <= 30){
     contador = contador + 1;
+    
+    Serial.print("\nDistancia: ");
+    Serial.print(distancia);
+    Serial.println(" cm");
+    Serial.print("Contador: ");
+    Serial.println(contador);
   }else{
     contador = contador - 1;
+    Serial.print("\nDistancia: ");
+    Serial.print(distancia);
+    Serial.println(" cm");
+    Serial.print("Contador: ");
+    Serial.println(contador);
   }
 }
