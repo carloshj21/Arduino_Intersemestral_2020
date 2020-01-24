2574025adfbe9b1ec10c7aa7c4ec6efb40404f1e
carloshj21 <carlos21p6-0800@comunidad.unam.mx>
2020-01-24 09:13:01 -0600

    Prender y apagar LED mediante comunicacíón serial

---
+++ Arduino Avanzado/Ejemplos/5. Corriente alterna/prenderApagarLEDSerial/prenderApagarLEDSerial.ino
@@ -0,0 +1,20 @@
+int led = 8;
+char estado;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    estado = Serial.read();
+
+    if(estado == 'y' || estado == 'Y'){
+      digitalWrite(led,HIGH);
+    }
+    else if(estado == 'n' || estado == 'N'){
+      digitalWrite(led,LOW);
+    }
+  }
+}
